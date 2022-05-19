2d27d4de3c9bfeb254d2c616b6886b6105abfbc6
Abel Akponine <abelakponine@gmail.com>
2022-05-19 15:00:01 +0100

    React Vs Pokeapi

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // -v 6.3.0
-import Home from "./views/home";
+import Home from "./views/home"
 
 function App() {
+  
   return (
     <Router>
       <Routes>
         <Route path={"/"} element={<Home/>} exact></Route>
+        <Route path={"/:name"} element={<Home/>} exact></Route>
       </Routes>
     </Router>
   );
 }
+
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Home from './views/home';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders learn react link', async () => {
+  
+  render(<Home/>);
+  
+  const linkElement = await screen.findByText((con, elem)=>{
+    return elem.id == 'pokemon';
+  });
+  console.log(linkElement.querySelector('label').textContent)
+  expect(linkElement.querySelector('label').textContent.toLowerCase()).toBe('pikachu');
 });
